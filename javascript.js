7369c3b3a5557bd9bb6eb7468857fe6515c6eb83
Stephen MS <stephen.mcilroy.sharpe@gmail.com>
2021-12-15 12:03:19 +0000

    Added color and eraser functionality, rainbowMode in progress

--- javascript.js
+++ javascript.js
@@ -1,14 +1,31 @@
+let rainbowColors = ['green', 'yellow', 'rebeccapurple'];
+
+//get variables from webpage
+
 const gridContainer = document.querySelector('#gridContainer');
 
 const slider = document.querySelector('.slider');
 
 let gridValue = slider.value;
 
+const colorPicker = document.querySelector('.colorPicker');
+
+let userColor = colorPicker.value;
+
+const colorMode = document.querySelector('.colorMode');
+
+const rainbowMode = document.querySelector('.rainbowMode');
+
+const eraser = document.querySelector('.eraser')
+
 const sliderText = document.querySelector('.sliderText');
 
 sliderText.textContent = `${slider.value} X ${slider.value}`;
 
+//add listeners for page load and user inputs
+
 window.addEventListener('load', () => {
+    colorMode.classList.add('selectedButton');
     gridValue = slider.value;
     makeGrid(gridValue);
     
@@ -26,8 +43,41 @@ slider.addEventListener('change', () => {
     
 })
 
+colorPicker.addEventListener('change', () =>{
+    if (colorMode.classList.contains('selectedButton')) {
+        userColor = colorPicker.value;
+    }
+    sketching(userColor);
+    
+})
+
+colorMode.addEventListener('click', () => {
+    eraser.classList.remove('selectedButton');
+    colorMode.classList.add('selectedButton');
+    rainbowMode.classList.remove('selectedButton');
+    userColor = colorPicker.value;
+    sketching(userColor);
+})
+
+eraser.addEventListener('click', () => {
+    eraser.classList.add('selectedButton');
+    colorMode.classList.remove('selectedButton');
+    rainbowMode.classList.remove('selectedButton');
+    userColor = 'white';
+    sketching(userColor);
+})
+
+rainbowMode.addEventListener('click', () => {
+    eraser.classList.remove('selectedButton');
+    colorMode.classList.remove('selectedButton');
+    rainbowMode.classList.add('selectedButton');
+    userColor = rainbowColors[2];
+    sketching(userColor);
+})
 
 
+
+//operation functions
 function makeGrid(gridValue) {    
     while (gridContainer.firstChild){
     gridContainer.removeChild(gridContainer.lastChild)
@@ -43,24 +93,27 @@ function makeGrid(gridValue) {
         gridContainer.appendChild(gridRow);
 
     }
-    sketching();
+    sketching(userColor);
 }
 
-function sketching() {
-    console.log('Im getting squares');
+function sketching(userColor) {
+    
     const squares = document.querySelectorAll('.square');
     squares.forEach((square) => {
         square.addEventListener('mouseenter', () => {
-            square.classList.add('selected');
+            if (rainbowMode.classList.contains('selectedButton')) {   //Tried to add a random selection from rainbowColors doesn't appear to work. Appears to be only selecting rainbowColors[0]
+                userColor = rainbowColors[(Math.floor(Math.random()))*3];
+            }
+            square.style.backgroundColor = userColor
         })
     })
+    
 
     const clear = document.querySelector('.reset');
     clear.addEventListener('click', () => {
         squares.forEach((square) => {
-            square.classList.remove('selected');
+            square.style.backgroundColor = 'white';
     })
 })
 }
-
 
